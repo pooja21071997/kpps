376fe6453ee722afc5b63b8b7abf8e4979c63f40
poojaPersonal <100690066+pooja21071997@users.noreply.github.com>
2026-01-31 16:19:18 +0530

    navbar issue fixed

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
-
+import { Collapse } from 'bootstrap';
 @Component({
   selector: 'app-navbar',
   imports: [RouterLink],
@@ -8,5 +8,12 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   styleUrl: './navbar.css',
 })
 export class Navbar {
+closeNavbar() {
+    const navbar = document.getElementById('mainNavbar');
+    if (navbar && navbar.classList.contains('show')) {
+      const bsCollapse = new Collapse(navbar);
+      bsCollapse.hide();
+    }
+  }
 
 }
